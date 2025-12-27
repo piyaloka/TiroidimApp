6f55da4232ba3da8915f23152db35a1f47d5dc60
Tutku35 <tutku.kirikcioglu@hotmail.com>
2025-12-27 13:57:30 +0300

    Update artibutonu_ekrani.py

--- screens/artibutonu_ekrani.py
+++ screens/artibutonu_ekrani.py
@@ -57,48 +57,23 @@ class ArtiButonuEkrani(MDScreen):
         print("İlaç atlandı.")
 
 """
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.button import MDRaisedButton
-from kivymd.uix.label import MDLabel
-from database import Database
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.animation import Animation
 
-class ArtiButonuEkrani(MDScreen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.db = Database()
-        self.md_bg_color = (1, 1, 1, 1)
+MDFloatingActionButton:
+        id: main_fab
+        icon: "plus"
+        md_bg_color: 0.4, 0.1, 0.9, 1
+        pos_hint: {"center_x": .85, "center_y": .1}
+        on_release: app.toggle_menu()
 
-        layout = MDBoxLayout(
-            orientation="vertical",
-            padding=30,
-            spacing=20,
-            pos_hint={"top": 1}
-        )
-
-        layout.add_widget(MDLabel(text="Yeni İlaç Ekle", halign="center", font_style="H5", size_hint_y=None, height=50))
-
-        self.drug_name = MDTextField(hint_text="İlaç Adı", mode="rectangle")
-        self.dosage = MDTextField(hint_text="Dozaj (ör: 500 mg)", mode="rectangle")
-        self.frequency = MDTextField(hint_text="Kullanım Sıklığı", mode="rectangle")
-
-        save_button = MDRaisedButton(
-            text="KAYDET",
-            md_bg_color=(0.42, 0, 0.95, 1),
-            pos_hint={"center_x": 0.5},
-            on_release=self.ilac_kaydet
-        )
-
-        layout.add_widget(self.drug_name)
-        layout.add_widget(self.dosage)
-        layout.add_widget(self.frequency)
-        layout.add_widget(save_button)
-        layout.add_widget(MDLabel()) # Boşluk
-
-        self.add_widget(layout)
-
-    def ilac_kaydet(self, instance):
-        # Şimdilik sadece Dashboard'a dönelim
-        print(f"Kaydediliyor: {self.drug_name.text}")
-        self.manager.current = "dashboard"
+    # --- DİNAMİK FORM SAYFASI ---
+    MDCard:
+        id: form_page
+        pos_hint: {"center_x": 0.5, "center_y": -1} # Ekranın çok dışında başlar
+        size_hint: 0.95, 0.8
+        radius: [30, 30, 30, 30]
+        elevation: 12
+        padding: "20dp"
+        orientation: 'vertical'
